dd1e65ab40da52d8f1e3a6733a5cff49561d02d9
Andreas Österreicher <oesi@technikum-wien.at>
2019-02-01 11:51:35 +0100

    Neue View für genehmigte Budgets hinzugefügt

---
+++ sql/1/005_vw_budget_genehmigt.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW extension.vw_budget_genemigt AS
+SELECT 
+	tbl_budget_antrag.kostenstelle_id, tbl_budget_antrag.geschaeftsjahr_kurzbz, tbl_budget_antrag.bezeichnung,
+	tbl_budget_position.budgetposition_id, tbl_budget_position.budgetposten, tbl_budget_position.konto_id, 
+	tbl_budget_position.betrag, tbl_budget_position.kommentar, tbl_budget_position.projekt_id
+FROM
+	extension.tbl_budget_antrag
+	JOIN extension.tbl_budget_position USING(budgetantrag_id)
+WHERE
+	EXISTS(
+		SELECT 
+			1 
+		FROM 
+			extension.tbl_budget_antrag_status 
+		WHERE 
+			budgetantrag_id=tbl_budget_antrag.budgetantrag_id 
+			AND budgetstatus_kurzbz='approved'
+	)

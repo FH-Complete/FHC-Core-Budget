aaf35744191e62eb2e5190f43bf694bca3184cb0
Andreas Österreicher <oesi@technikum-wien.at>
2019-12-02 14:17:46 +0100

    Budget View angepasst damit der Freigabestatus korrekt ermittelt wird wenn das Budget mehrfach Freigegeben und zurückgesetzt wird

--- sql/1/005_vw_budget_approved.sql
+++ sql/1/005_vw_budget_approved.sql
@@ -1,18 +1,18 @@
 CREATE OR REPLACE VIEW extension.vw_budget_approved AS
-SELECT 
+SELECT
 	tbl_budget_antrag.kostenstelle_id, tbl_budget_antrag.geschaeftsjahr_kurzbz, tbl_budget_antrag.bezeichnung,
-	tbl_budget_position.budgetposition_id, tbl_budget_position.budgetposten, tbl_budget_position.konto_id, 
+	tbl_budget_position.budgetposition_id, tbl_budget_position.budgetposten, tbl_budget_position.konto_id,
 	tbl_budget_position.betrag, tbl_budget_position.kommentar, tbl_budget_position.projekt_id
 FROM
 	extension.tbl_budget_antrag
 	JOIN extension.tbl_budget_position USING(budgetantrag_id)
 WHERE
-	EXISTS(
+	'approved'= (
 		SELECT 
-			1 
-		FROM 
+			budgetstatus_kurzbz
+		FROM
 			extension.tbl_budget_antrag_status
-		WHERE 
-			budgetantrag_id=tbl_budget_antrag.budgetantrag_id 
-			AND budgetstatus_kurzbz='approved'
+		WHERE
+			budgetantrag_id=tbl_budget_antrag.budgetantrag_id
+		ORDER BY datum desc LIMIT 1
 	)
